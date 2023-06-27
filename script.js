dd8701fe2e85f0def2cc0195f9f4c3de245bd361
Yiseo Kwon <yiseo1122@gmail.com>
2023-06-27 02:19:24 -0500

    feat: Create prompts and add if statements that match the criteria

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// create a password that meets the selected criteria
+function generatePassword(){
+  // present with a series of prompts for password criteria
+  let length = parseInt(prompt("Enter the length of the password (between 8 and 128 characters)"));
+  let includeLower = confirm("Does the character include lowercase?");
+  let includeUpper = confirm("Does the character include uppercase?");
+  let includeNum = confirm("Does the character include numeric?");
+  let includeSpecial = confirm("Does the character include special characters?");
+
+  if(Number.isNaN(length) || length < 8 || length > 128){
+    window.alert("You must enter the number between 8 and 128 characters");
+    return;
+  }
+
+  if (!length && !includeLower && !includeUpper && !includeNum && !includeSpecial){
+    window.alert("You must select at least one character type.");
+    return;
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
