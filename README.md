29dc612aad2e067275b6d7bfa7ff5402b81323a3
Yiseo Kwon <yiseo1122@gmail.com>
2023-07-03 14:29:02 -0500

    docs: Modify README.file and create an assets folder

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # pwd-challenge-3
-This project was created to make a password generator
+
+## Description
+This project was created to make a password generator application that allows
+employees to generate random passwords according to their chosen criteria.
+
+If the user requests to generate a password that does not meet the criteria, an alert will be displayed to allow them to make a new selection according to the criteria.
+
+## Live link
+[**https://yiseo.github.io/pwd-challenge-3/**](https://yiseo.github.io/pwd-challenge-3/)
+
+
+## Screenshot
+![This webpage includes a generate password button and the password exposes to the screen](./assets/image/screenshot.png)
+
+## Installation
+N/A
+
+## Technologies Used
+- HTML
+- CSS
+- Javascript
+- Github
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 // create a password that meets the selected criteria
 function generatePassword(){
   // present with a series of prompts for password criteria
-  let length = parseInt(prompt("Enter the length of the password (between 8 and 128 characters)"));
+  let length = parseInt(prompt("Enter the length of the password (between 8 and 128 characters)")); //integer
   let includeLower = confirm("Does the character include lowercase?");
   let includeUpper = confirm("Does the character include uppercase?");
   let includeNum = confirm("Does the character include numeric?");
@@ -14,10 +14,40 @@ function generatePassword(){
     return;
   }
 
-  if (!length && !includeLower && !includeUpper && !includeNum && !includeSpecial){
+  console.log(!includeLower, !includeUpper, !includeNum, !includeSpecial)
+  if (!includeLower && !includeUpper && !includeNum && !includeSpecial){
     window.alert("You must select at least one character type.");
     return;
   }
+
+  // Setting the list of characters for the password
+  let lowercaseLi = "abcdefghijklmnopqrstuvwxyz";
+  let uppercaseLi = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let numericLi = "0123456789";
+  let incSpecialCharacterLi = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  let character = "";
+  if(includeLower == true){
+    character += lowercaseLi;
+  }
+  if(includeUpper == true){
+    character += uppercaseLi;
+  }
+  if(includeNum == true){
+    character += numericLi;
+  }
+  if(includeSpecial == true){
+    character += incSpecialCharacterLi;
+  }
+
+  let result = "";
+  let characterLength = character.length;
+  for (let i = 0; i < length; i++){
+    // charAt() returns the character at a specified index
+    result += character.charAt(Math.floor(Math.random() * characterLength));
+  }
+
+  return result;
 }
 
 // Write password to the #password input
